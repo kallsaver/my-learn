8a0f3fbeba770413f356c681f030fc0f89287383
kallsaver@163.com <kallsaver@163.com>
2017-05-12 18:01:02 +0800

    vue 512

--- vue-todos/src/App.vue
+++ vue-todos/src/App.vue
@@ -1,60 +1,98 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1></h1>
-    <h2>Essential Links</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <li v-for="(item,index) in todoList ">
+        <!--b这个类名可以在template中使用-->
+        <label v-bind:class="{'b':item.done}">{{index+1}}啦啦啦{{item.value}}</label>
+        <time>{{item.created | date }}</time>
+      </li>
     </ul>
-    <h2>Ecosystem</h2>
     <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li v-for="(value,key) in obj">
+        <!--v-bind:style="json格式"-->
+        <p v-bind:style="{'color':'#fff'}">{{key}}:{{value}}</p>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+import moment from 'moment';
+// 运行
+import 'moment/locale/zh-cn';
+moment.locale('zh-cn')
+
+// es6属性如果是方法的简写
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      title : 'vue-todos',
+      todoList : [
+        {
+          value : 'angular',
+          done : true,
+          created : Date.now()
+        },
+        {
+          value : 'react',
+           done : false,
+           created : Date.now()
+        },
+        {
+          value : 'vue',
+          done : true,
+          created : Date.now()
+        }
+      ],
+      obj : {
+        first : 'ray',
+        second : 'dpi'
+      }
+    }
+  },
+  filters : {
+    date(val){
+      return moment(val).calendar()
     }
   }
 }
+// 等同于
+  // export default {
+  //   name : 'app',
+  //   // data在vue中是挂载属性,而函数挂载在computed节点上
+  //   // 并且如果data挂载的是一个函数,那么这个函数会在挂载的过程运行
+  //   data : function(){
+  //     console.log('运行');
+  //     return {
+  //       msg : 'welcome to vue'
+  //     }
+  //   }
+  // }
+// 等同于下面,然而会报错
+  // export default {
+  //   name : 'app',
+  //   data : {
+  //     msg : 'welcome '
+  //   }
+  // }
+
+
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style lang="less" scoped>
+// 不需要在webpack.config.js中设置,只需要npm install less-loader --save-dev
+  @import (reference)'./less/common.less';
+  *{margin:0;padding:0;}
+  li{list-style:none;margin:0;padding:0;}
+  .b{color:#fff}
+  #app{
+    ul{
+      li{
+        .a;text-align: center;line-height:50px;
+        height:50px;margin-bottom:20px;
+      }
+    }
+  }
 </style>
