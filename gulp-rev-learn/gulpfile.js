8eb4123c5ba7fe9135935d68b4543bb0a69a65ab
kallsave <415034609@qq.com>
2017-09-05 01:44:45 +0800

    20170905

--- gulp-rev-learn/gulpfile.js
+++ gulp-rev-learn/gulpfile.js
@@ -3,28 +3,28 @@ var gulp         = require('gulp');
 var browserSync  = require('browser-sync').create();
 var less         = require('gulp-less');
 var plumber      = require('gulp-plumber');
-var postcss      = require('gulp-postcss');
 //不是所有的前缀补全都支持,比如涉及伪元素的input::input-placeholder不支持,还是要手动写
 var autoprefixer = require('gulp-autoprefixer');
 // cleancss是minifycss的升级版,但是有路径问题,不推荐使用
-var cleancss     = require('gulp-clean-css');
 var minifycss    = require('gulp-minify-css');
 var concat       = require('gulp-concat');
 var rev          = require('gulp-rev');
 var uglify       = require('gulp-uglify');
+var merge        = require('gulp-merge');
+var rename       = require('gulp-rename');
 
 
 gulp.task('less',function(){
-	return gulp.src('src/less/**/*')
-	.pipe(plumber({
-		errorHandler : function(){
-			this.emit('end');
-		}
-	}))
-	.pipe(less())
-	.on('error',function(err){
-		console.log('Error: ',err.message);
-	})
+    return gulp.src('src/less/**/*')
+    .pipe(plumber({
+        errorHandler : function(){
+            this.emit('end');
+        }
+    }))
+    .pipe(less())
+    .on('error',function(err){
+        console.log('Error: ',err.message);
+    })
     .pipe(autoprefixer())
     .pipe(concat('all.css')) 
     .pipe(minifycss({
@@ -34,19 +34,29 @@ gulp.task('less',function(){
         compatibility: 'ie7',
         // 保留autoprefixer的特殊前缀
     }))
-	.pipe(gulp.dest('dist/css'))
+    .pipe(rename(function(path){
+        path.basename += '.min';
+    }))
+    .pipe(gulp.dest('dist/css'))   
 })
 
 gulp.task('js',function(){
-	return gulp.src('src/js/**/*')
+    return gulp.src('src/js/**/*')
     .pipe(concat('all.js'))
-    .pipe(uglify()) 
-	.pipe(gulp.dest('dist/js'))
+    .pipe(uglify())
+    .pipe(rename(function(path){
+        path.basename += '.min';
+    }))
+    .pipe(gulp.dest('dist/js')) 
 })
 
-gulp.task('bulid',function(){
-    return gulp.src('dist/**/*')
-    .pipe()
+gulp.task('build',function(){
+    gulp.src('dist/**/*')
+    .pipe(rev())
+    .pipe(gulp.dest('dist/'))
+    // 会在dist目录生成rev-manifest.json文件
+    .pipe( rev.manifest() )
+    .pipe(gulp.dest('dist/'))
 })
 
 // Static Server + watching scss/html files
@@ -60,10 +70,12 @@ gulp.task('serve',['less','js'],function() {
         notify : false
     });
 
-    gulp.watch('less/**/*',['less']);
-    gulp.watch('css/**/*').on('change', browserSync.reload);;
+    gulp.watch('src/less/**/*',['less']);
+    gulp.watch("src/js/**",['js']);
+    gulp.watch('dist/js/**/*').on('change', browserSync.reload);
+    gulp.watch('dist/css/**/*').on('change', browserSync.reload);
     gulp.watch("views/**").on('change', browserSync.reload);
-    gulp.watch("js/**").on('change', browserSync.reload);
+    
 });
 
 
