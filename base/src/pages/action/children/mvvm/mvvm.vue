1f4fc21d4c4e68a79148930557b910b5e7734f87
kallsave <415034609@qq.com>
2019-01-08 19:15:48 +0800

    2019-01-08

---
+++ base/src/pages/action/children/mvvm/mvvm.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="mvvm"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- koa-learn/index.js
+++ koa-learn/index.js
@@ -6,24 +6,24 @@ const path = require('path');
 const port = 8777;
 const app = new Koa();
 
+app.use(views(__dirname + '/views', {
+  extension: 'ejs'
+}));
+
 const homeRouter = new Router();
 const userRouter = new Router();
 
 homeRouter.get('/', async (ctx, next) => {
   await ctx.render('home', {
     title: 'Hello Koa 2!'
-  })
+  });
 });
 
 userRouter.get('/user', async (ctx, next) => {
   await ctx.render('user', {
     title: 'user!'
-  })
+  });
 });
-
-app.use(views(__dirname + '/views', {
-  extension: 'ejs'
-}));
 
 app.use(homeRouter.routes(), homeRouter.allowedMethods());
 app.use(userRouter.routes(), userRouter.allowedMethods());
