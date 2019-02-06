c6f16bbb49c57020e44394b3fbe042ae44102f9a
kallsave <415034609@qq.com>
2019-02-06 13:13:11 +0800

    2019-02-26

--- babel-learn/src/script.js
+++
@@ -1,4 +0,0 @@
-console.log(1);
-//保存时编译错误,使用plumber插件可以阻止任务编译
-//不用跳出进程重新gulp回车
-console.log(2)
---
+++ vue-css/src/pages/base/children/test/test.vue
@@ -0,0 +1,49 @@
+<template>
+  <base-page
+    :title="$route.name">
+    <div class="index">
+      <base-vhtml :context="context"></base-vhtml>
+    </div>
+  </base-page>
+</template>
+
+<script>
+import BaseVhtml from '@/base-components/vhtml/vhtml.js'
+
+export default {
+  components: {
+    BaseVhtml,
+  },
+  data() {
+    return {
+      context: `<div id="dt" class="context">
+                  <ul>
+                    <li>123</li>
+                    <li>456</li>
+                  </ul>
+                  <img src="${require('@/assets/images/bg01.jpg')}" />
+                </div>`
+    }
+  },
+  computed: {
+  },
+  watch: {
+  },
+  mounted() {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.index
+  width: 100%
+  height: 100vh
+  padding: 100px
+
+.context
+  width: 200px
+  height: 200px
+  background: peru
+</style>
