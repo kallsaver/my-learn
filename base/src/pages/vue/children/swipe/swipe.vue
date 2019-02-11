155838407a7057657ad4235c925496410f0d4871
kallsave <415034609@qq.com>
2019-02-11 18:10:06 +0800

    base

---
+++ base/src/pages/vue/children/swipe/swipe.vue
@@ -0,0 +1,18 @@
+<template>
+  <page
+    :title="$route.meta.title">
+    <div class="swipe">
+
+    </div>
+  </page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
