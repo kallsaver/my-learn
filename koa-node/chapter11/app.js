8c0f0ea064a58f75b0cd1bd4ce9c56396fbeb6cb
kallsave <415034609@qq.com>
2019-03-01 17:45:50 +0800

    Revert "分支标识"
    This reverts commit fb60b67f253c11379881e70d2483c8cec330c408.

--- koa-node/chapter11/app.js
+++ koa-node/chapter11/app.js
@@ -1,79 +1,89 @@
+const path = require('path')
 const Koa = require('koa')
+const bodyParser = require('koa-bodyparser')
+const static = require('koa-static')
+const views = require('koa-views')
 const Router = require('koa-router')
-const Http = require('http')
-const Querystring = require('querystring')
-const Request = require('request')
+const { sign } = require('jsonwebtoken')
+
+// 设置密匙
+const secret = 'demo'
+const jwt = require('koa-jwt')({secret})
 
 const app = new Koa()
 const router = new Router()
 
-const Service = {
-  search: async (kw = '') => {
-    return new Promise((resolve, reject) => {
-      Http.request({
-        hostname: 'm.maoyan.com',
-        path: '/ajax/search?' + Querystring.stringify({
-          kw,
-          cityId: 10
-        })
-      }, (res) => {
-        // console.log(res)
-        // 把 buffer变成uft8字符串
-        res.setEncoding('utf8')
-
-        let str = ''
-
-        res.on('data', (chunk) => {
-          str += chunk
-        }).on('end', () => {
-          resolve(str)
-        })
-
-      }).end()
-
-    })
-  }
-}
-
-const Render = (data = {}, kw = '') => {
-
-  let arr = [
-    '<style>',
-    '*{margin:0;padding:0;font-size: 12px;}',
-    'body{padding:10px}',
-    'button{padding: 0 10px;line-height: 20px;margin-left: 10px;}',
-    'input{line-height: 20px; width: 200px;padding: 0 5px;}',
-    'h3{font-size: 14px;}',
-    'i{font-size: 14px;font-style: inherit;color: #f80;}',
-    'p{color:#666;}',
-    '.item{padding:10px 0 10px 0;border-bottom:1px solid #d2d2d2;display:flex;}',
-    '.info{margin-left:10px}',
-    '</style>',
-    '<form><input name="kw" value="' + kw + '"/><button>搜索</button></form>'
-  ]
-
-  data.movies && data.movies.list.forEach(item => {
-    arr.push('<div class="item">')
-    arr.push('<img src="' + item.img.replace('w.h', '64.90') + '"/>')
-    arr.push('<div class="info">')
-    arr.push('<h3>' + item.nm.replace(new RegExp(kw, 'g'), '<i>' + kw + '</i>') + '（' + item.enm + '）' + item.ver + '</h3>')
-    arr.push('<p>' + item.cat + '</p>')
-    arr.push('<p>' + item.star + '</p>')
-    arr.push('<p>' + item.pubDesc + '</p>')
-    arr.push('<p>' + item.sc + '分</p>')
-    arr.push('</div>')
-    arr.push('</div>')
-  })
-
-  return arr.join('')
-}
-
-router.get('/', async (ctx, next) => {
-  let { kw } = ctx.query
-  let data = await Service.search(kw)
-  ctx.body = Render(JSON.parse(data), kw)
+// 模板引擎
+app.use(views(path.join(__dirname + '/views')), {
 })
 
-app.use(router.routes()).listen(8080, () => {
-  console.log('Server is running at http://localhost:8080')
+// 静态资源
+app.use(static(path.join(__dirname, 'static')))
+
+
+router.get('/', async(ctx, next) => {
+  await ctx.render('index')
+  next()
+})
+
+router.post('/api/login', async(ctx, next) => {
+  ctx.response.status = 200
+  // koa-bodyparser的处理
+  let user = ctx.request.body
+  if (user && user.userName) {
+    let payload = {
+      username: user.userName
+    }
+    // 生成token
+    const token = sign(
+      payload,
+      secret,
+      {
+        // 以秒表示或描述时间跨度
+        expiresIn: 3
+      }
+    )
+
+    ctx.body = {
+      code: 1,
+      msg: 'success',
+      token: token
+    }
+  }
+})
+
+// 管理员中间件
+const admin = (() => {
+  return async (ctx, next) => {
+    // console.log(ctx.request)
+    next()
+  }
+})()
+
+
+router.get('/api/userInfo', jwt, async (ctx, next) => {
+  ctx.response.status = 200
+  // let query = ctx.request.query
+  // console.log(ctx.request)
+  ctx.body = {
+    code: 1,
+    msg: 'success',
+    data: {
+      name: 'a'
+    }
+  }
+})
+
+router.all('/*' ,async(ctx, next) => {
+  ctx.set('Access-Control-Allow-Origin', '*')
+  next()
+})
+
+
+app.use(bodyParser())
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen(3000, () => {
+  console.log('server is running at http://localhost:3000')
 })
