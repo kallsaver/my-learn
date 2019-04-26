62722cb79d3de5c3ac7a092347ca7e13b48bff0d
kallsave <415034609@qq.com>
2019-04-26 19:42:49 +0800

    drawImage

--- live-2d/live2d/js/message.js
+++ live-2d/live2d/js/message.js
@@ -101,13 +101,14 @@ initTips();
     showMessage(text, 12000);
 })();
 
-window.setInterval(showHitokoto,30000);
 
 function showHitokoto(){
     $.getJSON('https://v1.hitokoto.cn/',function(result){
         showMessage(result.hitokoto, 5000);
     });
 }
+
+showHitokoto();
 
 function showMessage(text, timeout){
     if(Array.isArray(text)) text = text[Math.floor(Math.random() * text.length + 1)-1];
