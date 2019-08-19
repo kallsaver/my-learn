7b89961c62ca0946d288f43c92208bbed73315e6
kallsave <415034609@qq.com>
2019-08-19 17:35:41 +0800

    2019-08-19

--- canvas/流星雨/worker.js
+++ canvas/流星雨/worker.js
@@ -5,12 +5,11 @@ this.onmessage = function (event) {
   let width = data.width
   let height = data.height
   let pixelData = imageData.data
-  // let area = []
 
   let len = pixelData.length
   for (let i = 0; i < len; i += 4) {
     // 像素位置
-    let pixelIndex  = i / 4
+    let pixelIndex = i / 4
     let r = pixelData[i]
     let g = pixelData[i + 1]
     let b = pixelData[i + 2]
@@ -18,10 +17,10 @@ this.onmessage = function (event) {
     let y = pixelIndex / width
     // let a = pixelData[i + 3]
     if (r < 10 && g < 20 && b < 30 && (x < 320 || y > 540)) {
-      continue
+
     } else {
       pixelData[i + 3] = 0
     }
   }
-  this.postMessage({imageData})
+  this.postMessage({ imageData })
 }
