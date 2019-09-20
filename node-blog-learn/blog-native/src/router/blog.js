58d48079209dbe77362b58136d632b1857c53476
kallsave <415034609@qq.com>
2019-09-20 19:10:28 +0800

    2019-09-20

--- node-blog-learn/blog-native/src/router/blog.js
+++ node-blog-learn/blog-native/src/router/blog.js
@@ -1,36 +1,36 @@
 const handleBlogRouter = (req, res) => {
   const method = req.method
   const url = req.url
-  const path = req.split('?')[0]
+  const path = url.split('?')[0]
 
-  if (method === 'GET' && path === '/api/blog/list') {
-    return {
-      msg: '/api/blog/list'
+  // api接口
+
+  if (method === 'GET') {
+    if (path === '/api/blog/list') {
+      return {
+        msg: '/api/blog/list'
+      }
+    } else if (path === '/api/blog/detail') {
+      return {
+        msg: '/api/blog/detail'
+      }
+    }
+  } else if (method === 'POST') {
+    if (path === '/api/blog/new') {
+      return {
+        msg: '/api/blog/new'
+      }
+    } else if (path === '/api/blog/update') {
+      return {
+        msg: '/api/blog/new'
+      }
+    } else if (path === '/api/blog/delete') {
+      return {
+        msg: '/api/blog/new'
+      }
     }
   }
-
-  if (method === 'GET' && path === '/api/blog/detail') {
-    return {
-      msg: '/api/blog/detail'
-    }
-  }
-
-  if (method === 'POST' && path === '/api/blog/new') {
-    return {
-      msg: '/api/blog/new'
-    }
-  }
-
-  if (method === 'POST' && path === '/api/blog/update') {
-    return {
-      msg: '/api/blog/new'
-    }
-  }
-
-  if (method === 'POST' && path === '/api/blog/delete') {
-    return {
-      msg: '/api/blog/new'
-    }
-  }
-
+  return null
 }
+
+module.exports = handleBlogRouter
