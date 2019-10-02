8be3a86f2cae7c2ba9a135b28cbe53559b391561
kallsave <415034609@qq.com>
2019-10-03 00:43:19 +0800

    gulp-nodemon reload

--- node-blog-learn/blog-native/app.js
+++ node-blog-learn/blog-native/app.js
@@ -16,9 +16,22 @@ const serverHandler = (req, res) => {
   }
   // 未命中路由
   res.writeHead(404, {
-    'Content-type': 'text/plain;charset=utf-8'
+    'Content-type': 'text/html;charset=utf-8'
   })
-  res.write('404 Not Found')
+  let html = `<!DOCTYPE html>
+                <html lang="en">
+                  <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                    <title>Document</title>
+                  </head>
+                  <body>
+                    <div>404ggg</div>
+                  </body>
+                </html>
+              `
+  res.write(html)
   res.end()
 }
 
--- node-blog-learn/blog-native/gulpfile.js
+++ node-blog-learn/blog-native/gulpfile.js
@@ -1,27 +1,54 @@
 const gulp = require('gulp')
 const nodemon = require('gulp-nodemon')
 const browserSync = require('browser-sync').create()
-const reload = browserSync.reload
+
+const DELAY = 5000
+
+const nodemonWatch = ['src/router', 'app.js', 'bin/www.js']
+
+const DEFAULT_TIME_SLICE = 400
+
+function Debounce(timeSlice = DEFAULT_TIME_SLICE)  {
+  this.timeSlice = timeSlice
+}
+Debounce.prototype.run = function (func) {
+  if (typeof func === 'function') {
+    if (this.timer) {
+      clearTimeout(this.timer)
+    }
+    this.timer = setTimeout(func, this.timeSlice)
+  }
+}
+
+let debounce = new Debounce()
+
+const reload = function (time) {
+  time = time || 0
+  setTimeout(() => {
+    browserSync.reload()
+  }, time)
+}
+
+gulp.task('nodemon', (cb) => {
+  let isStart = false
+  return nodemon({
+    script: 'bin/www.js',
+    watch: nodemonWatch,
+  }).on('start', () => {
+    if (!isStart) {
+      cb()
+      isStart = true
+    }
+  }).on('restart', () => {
+    debounce.run(() => {
+      console.log('restart')
+      reload(DELAY)
+    })
+  })
+})
 
 // 开发时运行server
-// 在mac下会报错
-// gulp.task('server', () => {
-//   nodemon({
-//     script: 'bin/www.js',
-//     ignore: ['node_modules/', 'gulpfile.js']
-//   }).on('start', function () {
-//     browserSync.init({
-//       // 以ip地址的方式打开
-//       // open: 'external',
-//       port: 4006,
-//       proxy: `http://localhost:8067/`,
-//       notify: false,
-//     })
-//     gulp.watch('src/**/*.js').on('change', reload)
-//   })
-// })
-
-gulp.task('dev', () => {
+gulp.task('server', ['nodemon'], () => {
   browserSync.init({
     // 以ip地址的方式打开
     // open: 'external',
@@ -29,10 +56,14 @@ gulp.task('dev', () => {
     proxy: `http://localhost:8067/`,
     notify: false,
   })
-  gulp.watch('src/**/*.js').on('change', reload)
-  gulp.watch('app.js').on('change', reload)
+  gulp.watch('src/**/*.js').on('change', () => {
+    reload()
+  })
+  gulp.watch('src/views/*.html').on('change', () => {
+    reload()
+  })
 })
 
 gulp.task('default', () => {
-
+  console.log('default')
 })
