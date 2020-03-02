92d2cc223a06f392db45cd18d03d8cac926a9be9
kallsave <415034609@qq.com>
2020-03-02 17:54:17 +0800

    20200302

--- vue-base/src/pages/action/children/promise/promise.vue
+++ vue-base/src/pages/action/children/promise/promise.vue
@@ -3,11 +3,25 @@
 </template>
 
 <script>
-import Promise from './src/polyfill.js'
+import loadjs from 'loadjs'
 
 export default {
   mounted() {
-
+    function Promise() {}
+    loadjs([
+      './static/promise-polyfill.js'
+    ], { returnPromise: true }).then(() => {
+      const a = new window.ES6Promise((resolve) => {
+        resolve()
+      })
+      console.log(a)
+      window.setTimeout(() => {
+        console.log(11)
+      }, 0)
+      a.then(() => {
+        console.log(88)
+      })
+    })
   },
 }
 </script>
